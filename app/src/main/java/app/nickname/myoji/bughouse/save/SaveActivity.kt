e14207464ee10e2cd77bc493995082546d0de7e3
Kaito-Dogi <kendobu0405@gmail.com>
2021-06-27 16:23:52 +0900

    fix: ListActivity

--- app/src/main/java/app/nickname/myoji/bughouse/save/SaveActivity.kt
+++ app/src/main/java/app/nickname/myoji/bughouse/save/SaveActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import app.nickname.myoji.bughouse.R
 
 class SaveActivity : AppCompatActivity() {
@@ -16,10 +17,12 @@ class SaveActivity : AppCompatActivity() {
         val saveButton: Button = findViewById(R.id.save_button)
         val sharedPreferences = getSharedPreferences("Second", Context.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
+
         saveButton.setOnClickListener {
             val text = input.text.toString()
             editor.putString("SAVE", text)
             editor.apply()
+            Toast.makeText(baseContext, "${text}を保存しました", Toast.LENGTH_SHORT).show()
         }
 
     }
